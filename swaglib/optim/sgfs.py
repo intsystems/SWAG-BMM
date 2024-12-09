3e9703a052a6842524fa5c78ddf6d310d099942d
alshestt <81408581+alshestt@users.noreply.github.com>
2024-12-10 00:43:28 +0300

    Update sgfs.py

--- swaglib/optim/sgfs.py
+++ swaglib/optim/sgfs.py
@@ -1,73 +1,69 @@
+
 import torch
-import torch.linalg as linalg
-import numpy as np
 
-class SGFS(torch.optim.SGD):
 
-    def __init__(self, initial_lr=1.0e-5, B=None, **kwargs):
-        super(SGFS, self).__init__(**kwargs)
-        self.params['lr'] = initial_lr
-        if B is not None:
-            self.params['B'] = torch.tensor(B, dtype=torch.float32)
+class SGFS(torch.optim.Optimizer):
 
-    def _create_auxiliary_variables(self):
-        self.lr = torch.tensor(self.params['lr'], dtype=torch.float32)
-        self.n_weights = sum(np.prod(p.shape) for p in self.weights)
-        self.I_t = torch.zeros((self.n_weights, self.n_weights), dtype=torch.float32)
-        self.it = torch.tensor(1.0)
+    def __init__(self, params, lr=1.0e-2, h_max=None, mode='basic'):
+        self.lr = lr
+        self.h_max = h_max
+        self.mode = mode
+        self.gradients_list = []
+        self.E = None
+        self.param_size = None
+        super().__init__(params, defaults={})
 
-    def _get_updates(self):
-        n = self.params['batch_size']
-        N = self.params['train_size']
-        prec_lik = self.params['prec_lik']
-        prec_prior = self.params['prec_prior']
-        gc_norm = self.params['gc_norm']
-        gamma = float(n + N) / n
+    def compute_c(self):
+        gradients_tensor = torch.stack(self.gradients_list)
+        C = gradients_tensor.var(dim=0, unbiased=False)
 
-        # compute log-likelihood
-        error = self.model_outputs - self.true_outputs
-        logliks = log_normal(error, prec_lik)
-        sumloglik = logliks.sum()
+        return C
 
-        # compute gradient of likelihood wrt each data point
-        grads = torch.autograd.functional.jacobian(lambda weights: log_normal(self.model_outputs - self.true_outputs, prec_lik, weights), self.weights)
-        grads = torch.cat([g.view(-1) for g in grads], dim=0)
-        avg_grads = grads.mean(dim=0)
-        dist_grads = grads - avg_grads
+    def compute_h(self, C):
+        H = None
+        if self.mode == 'basic':
+            H = 2/len(self.gradients_list) * torch.inverse(self.lr**2 * C + self.E @ self.E.T)
+        elif self.mode == 'diagonal':
+            if self.h_max is not None:
+                one_vec = torch.ones(C.size(dim=0))
+                h_vec = self.h_max * one_vec
+                H = torch.diag(h_vec)
+            else:
+                B = torch.linalg.cholesky(C)
+                B_d = torch.diagonal(B)
+                E_d = torch.diagonal(self.E)
+                H_vec = 1 / (torch.mul(self.lr**2 * B_d * B_d, E_d * E_d))
+                H = 2 / len(self.gradients_list) * torch.diag(H_vec)
+        elif self.mode == 'scalar':
+            one_vec = torch.ones(C.size(dim=0))
+            B = torch.linalg.cholesky(C)
+            B_d = torch.diagonal(B)
+            E_d = torch.diagonal(self.E)
+            H_scalar = 2 * C.size(dim=0) / len(self.gradients_list) / torch.sum(torch.mul(self.lr**2 * B_d * B_d, E_d * E_d))
+            H = torch.diag(H_scalar * one_vec)
+        return H
 
-        # compute variance of gradient
-        var_grads = (1. / (n - 1)) * (dist_grads.T @ dist_grads)
 
-        logprior = log_prior_normal(self.weights, prec_prior)
-        grads_prior = torch.autograd.grad(logprior, self.weights, create_graph=True)
-        grads_prior = torch.cat([g.view(-1) for g in grads_prior])
+    def step(self, closure = None):
+        self.gradients_list = []
+        loss = None
+        if closure is not None:
+            loss = closure()
 
-        # update Fisher information
-        I_t_next = (1 - 1 / self.it) * self.I_t + (1 / self.it) * var_grads
+        for group in self.param_groups:
+            for param in group['params']:
+                if param.grad is not None:
+                    self.gradients_list.append(param.grad.data.clone())
+                    self.param_size = param.data.size()
 
-        # compute noise
-        B = self.params.get('B', gamma * I_t_next * N)
-        B_ch = linalg.cholesky(B)
-        noise = (2. / torch.sqrt(self.lr)) * B_ch @ torch.randn((self.n_weights, 1))
+        C = self.compute_c()
+        self.E = torch.eye(self.param_size)
+        H = self.compute_h(C)
+        for group in self.param_groups:
+            for param in group['params']:
+                if param.grad is not None:
+                    noise = torch.distributions.MultivariateNormal(torch.zeros(self.param_size), covariance_matrix=(self.E @ self.E.T))
+                    param.data -= self.lr**2 * H @ param.grad - self.lr * (H @ (self.E @ noise.sample()))
 
-        # expensive inversion
-        inv_cond_mat = gamma * N * I_t_next + (4. / self.lr) * B
-        cond_mat = linalg.inv(inv_cond_mat)
-
-        updates = []
-        updates.append((self.I_t, I_t_next))
-        updates.append((self.it, self.it + 1))
-
-        # update the parameters
-        updated_params = 2 * (cond_mat @ (grads_prior + N * avg_grads + noise.flatten()))
-        updated_params = updated_params.flatten()
-        last_row = 0
-        for p in self.weights:
-            sub_index = np.prod(p.shape)
-            up = updated_params[last_row:last_row + sub_index]
-            up = up.view(p.shape)
-            updates.append((p, up))
-            last_row += sub_index
-
-        return updates, sumloglik
+        return loss
 
