4cf64aa4aee4c0b0515499ef543995a0789e0f08
alshestt <81408581+alshestt@users.noreply.github.com>
2024-12-17 13:48:35 +0300

    Update sgfs_demo.py

--- swaglib/demo/sgfs_demo.py
+++ swaglib/demo/sgfs_demo.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import torch.optim as optim
 from swaglib.optim.sgfs import SGFS
 from torch.utils.data import DataLoader, TensorDataset
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 # Определяем модель
@@ -46,9 +49,6 @@ def demo_sgfs(batch_size=16):
 
 
     sample_optimizer = optim.SGD(model.parameters(), lr=0.01)
-
-
-
 
     # Обучаем модель
     num_epochs = 2000
@@ -95,10 +95,100 @@ def demo_sgfs(batch_size=16):
     #print(f'Model parameters: {list(model.parameters())}')
 
 
+
+def demo_sgfs2(batch_size):
+    # Загрузка датасета a9a
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+    column_names = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status',
+                    'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss',
+                    'hours-per-week', 'native-country', 'income']
+    data = pd.read_csv(url, header=None, names=column_names, na_values=' ?', skipinitialspace=True)
+
+    # Предобработка данных
+    data.dropna(inplace=True)
+    data['income'] = data['income'].apply(lambda x: 1 if x == '>50K' else 0)
+
+    # Преобразование категориальных переменных в числовые
+    data = pd.get_dummies(data, drop_first=True)
+
+    # Разделение на признаки и целевую переменную
+    X = data.drop('income', axis=1).values
+    y = data['income'].values
+
+    # Разделение на обучающую и тестовую выборки
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Стандартизация данных
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    # Преобразование данных в тензоры
+    X_train_tensor = torch.FloatTensor(X_train)
+    y_train_tensor = torch.FloatTensor(y_train).view(-1, 1)
+    X_test_tensor = torch.FloatTensor(X_test)
+    y_test_tensor = torch.FloatTensor(y_test).view(-1, 1)
+
+    # Определение модели Байесовой логистической регрессии
+    class BayesianLogisticRegression(nn.Module):
+        def __init__(self, input_dim):
+            super(BayesianLogisticRegression, self).__init__()
+            self.linear = nn.Linear(input_dim, 1)
+
+        def forward(self, x):
+            return torch.sigmoid(self.linear(x))
+
+    # Инициализация модели, функции потерь и оптимизатора
+    input_dim = X_train.shape[1]
+    model = BayesianLogisticRegression(input_dim)
+    dataset = TensorDataset(X_train_tensor, y_train_tensor)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)  # Создаем DataLoader
+    # Создаем модель и определяем функцию потерь
+    model = BayesianLogisticRegression(input_dim)
+    criterion = nn.BCELoss()
+    sample_optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+    # Обучаем модель
+    num_epochs = 20
+    for epoch in range(num_epochs):
+        model.train()  # Устанавливаем модель в режим обучения
+        # Словарь для накопления градиентов по всем батчам
+        all_gradients = {param: [] for param in model.parameters()}
+
+        # Проходим по всем батчам
+        for batch_x, batch_y in dataloader:
+
+            # Обнуляем градиенты перед каждой итерацией
+            sample_optimizer.zero_grad()
+
+            # Прямой проход
+            outputs = model(batch_x)
+
+            # Вычисление потерь
+            loss = criterion(outputs, batch_y)
+
+            # Обратный проход
+            loss.backward()
+
+            gradients_dict = {param: param.grad.detach().cpu() for param in model.parameters()}
+            for name in all_gradients.keys():
+                all_gradients[name].append(gradients_dict[name])
+
+        # Инициализируем оптимизатор SGFS
+        optimizer = SGFS(model.parameters(), lr=0.01, mode="basic", all_gradients=all_gradients)
+
+        # Шаг оптимизации
+        optimizer.step()
+
+        # print(all_gradients.keys())
+
+        # Выводим информацию об эпохе
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+
+
 if __name__ == '__main__':
-    demo_sgfs(batch_size=16)
+    #demo_sgfs2(batch_size=1024)
+    demo_sgfs(batch_size=32)
 
-    #model = SimpleModel()
-
-    #for name, param in model.named_parameters():
-        #print(f"Parameter: {name}, Size: {param.size()}")
+    #model = ComplexModel()
